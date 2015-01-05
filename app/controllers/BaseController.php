538bc5938bac3597d32d9c540d2de6d055a249d6
tzwm <wangjunzhe4444@gmail.com>
2015-01-05 04:59:26 -0800

    Initial commit.

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,32 @@
+<?php
+
+class BaseController extends Controller {
+
+	/**
+	 * Setup the layout used by the controller.
+	 *
+	 * @return void
+	 */
+	protected function setupLayout()
+	{
+		if ( ! is_null($this->layout))
+		{
+			$this->layout = View::make($this->layout);
+		}
+	}
+
+    protected function checkSignature($signature, $timestamp, $nonce, $token)
+    {
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
